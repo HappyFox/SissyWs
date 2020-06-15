b8d9e6a96c37c512defc1c0c23fe2639c4c7a0af
Keith Baston <keith.baston@gmail.com>
2020-06-14 18:09:07 -0700

    cleaning up main.js

--- static/main.js
+++ static/main.js
@@ -10,39 +10,7 @@ const pressedColor = getComputedStyle(document.documentElement).getPropertyValue
 
 const touchManager = new TouchManager()
 
-/*
-const on_move_handlers = new Map()
-const on_stop_handlers = new Map()
-
-function on_touch (event) {
-  const touches = event.changedTouches
-
-  console.log(event.type)
-
-  let handlers = null
-  if (event.type === 'touchend') {
-    event.preventDefault()
-    handlers = on_stop_handlers
-  } else if (event.type === 'touchmove') {
-    handlers = on_move_handlers
-  } else {
-    console.log('returning!')
-    return
-  }
-
-  for (let idx = 0; idx < touches.length; idx++) {
-    const touch = touches[idx]
-    if (handlers.has(touch.identifier)) {
-      handlers.get(touch.identifier)(touch)
-    }
-  }
-}
-
-document.addEventListener('touchmove', on_touch, false)
-document.addEventListener('touchend', on_touch, false)
-*/
-
-function handle_face_btn (event) {
+function handleFaceButton (event) {
   if (current !== null) {
     current.style.backgroundColor = null
   }
@@ -61,35 +29,34 @@ function start_look (target) {
   target.style.backgroundColor = pressedColor
 }
 
-function stop_look (touch) {
+function stopLook (touch) {
   const target = touch.target
   const direction = target.textContent
   console.log('Stop looking : ' + direction)
   target.style.backgroundColor = null
 }
 
-
-function handle_look_touch_start (event2) {
+function handleLookTouchStart (event2) {
   console.log('TOUCH START!')
   console.log(event2.changedTouches[0].identifier)
   event2.preventDefault()
 
   const target = event2.target
   start_look(target)
-  
+
   const touch = event2.changedTouches[0]
 
-  touchManager.register(touch.identifier, { onStop:stop_look })
-  //function handle_stop (touch) {
+  touchManager.register(touch.identifier, { onStop: stopLook })
+  // function handle_stop (touch) {
   //  console.log(touch.identifier)
-  //  stop_look(target)
+  //  stopLook(target)
   //  on_stop_handlers.delete(touch.identifier)
-  //}
+  // }
 
-  //on_stop_handlers.set(event.changedTouches[0].identifier, handle_stop)
+  // on_stop_handlers.set(event.changedTouches[0].identifier, handle_stop)
 }
 
-function handle_look_touch_end (event) {
+function handleLookTouchEnd (event) {
   console.log('TOUCH END!')
   console.log(event.changedTouches[0].identifier)
   event.preventDefault()
@@ -97,11 +64,11 @@ function handle_look_touch_end (event) {
 
 function bind_look_handers (button, idx, array) {
   console.log('binding ! : ' + button)
-  button.addEventListener('touchstart', handle_look_touch_start, false)
-  // button.onmousedown = handle_look_touch_start;
+  button.addEventListener('touchstart', handleLookTouchStart, false)
+  // button.onmousedown = handleLookTouchStart;
 
-  // button.addEventListener("touchend", handle_look_touch_end, false);
-  // button.addEventListener("touchcancel", handle_look_touch_end, false);
+  // button.addEventListener("touchend", handleLookTouchEnd, false);
+  // button.addEventListener("touchcancel", handleLookTouchEnd, false);
 }
-faceButtons.forEach((element, _, __) => element.onclick = handle_face_btn)
+faceButtons.forEach((element, _, __) => element.onclick = handleFaceButton)
 lookButtons.forEach(bind_look_handers)
