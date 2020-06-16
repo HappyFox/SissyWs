21baeb71f28faaf16ffd016a5eb1e604ef22b12b
Keith Baston <keith.baston@gmail.com>
2020-06-15 22:07:53 -0700

    Got multi-touch and mouse compatable buttons working.

--- static/button.js
+++ static/button.js
@@ -1,40 +1,129 @@
 
-export class Button {
-  constructor (element, {onDown = undefined, onUp = undefined, onMove = undefined } ) {
+function inSideElement(element, x, y) {
+  const rect = element.getBoundingClientRect()
+  if (x > rect.x && x < rect.right) {
+    if (y > rect.y && y < rect.bottom) {
+      return true
+    }
+  }
+  return false
+}
+
+export class ClickButton {
+  constructor (element, onClick) {
     this.element = element
-    
+    this.onClick = onClick
+
+    this.pointerId = undefined
+
+    this.upHandler = (event) => this.onPointerUp(event)
+    this.element.addEventListener('pointerdown', (event) => this.onPointerDown(event), false)
+
+    this.element.oncontextmenu = function (event) {
+      event.preventDefault()
+      event.stopPropagation()
+    }
+  }
+
+  onPointerDown (event) {
+    event.preventDefault()
+    event.target.setPointerCapture(event.pointerId)
+    this.pointerId = event.pointerId
+
+    this.element.addEventListener('pointerup', this.upHandler, false)
+    this.element.addEventListener('pointercancel', this.upHandler, false)
+  }
+
+  onPointerUp (event) {
+    if (this.pointerId !== event.pointerId) {
+      console.log('returning early !')
+      return
+    }
+    this.pointerId = undefined
+
+    event.preventDefault()
+    console.log('UP!!!')
+
+    event.stopPropagation()
+    // this.element.releasePointerCapture(event.pointerId)
+    this.element.removeEventListener('pointerup', this.upHandler, false)
+    this.element.removeEventListener('pointercancel', this.upHandler, false)
+
+    if (inSideElement(this.element, event.pageX, event.pageY)) {
+      this.onClick(event)
+    }
+  }
+}
+
+export class HoldButton {
+  constructor (element, onDown = undefined, onUp = undefined, onMove = undefined) {
+    this.element = element
+    this.pointerId = undefined
+
     this.onDown = onDown
     this.onUp = onUp
     this.onMove = onMove
 
-    this.upHandler = undefined
-    this.moveHandler = undefined
-    
-    this.element.addEventListener('pointerdown', (event) => this.onPointerDown(event), false)
-  }
-
-  onPointerDown (event) {
-    console.log("DOWN") 
     this.upHandler = (event) => this.onPointerUp(event)
     this.moveHandler = (event) => this.onPointerMove(event)
 
-    this.element.addEventListener('pointerup', this.upHandler , false)
+    this.element.addEventListener('pointerdown', (event) => this.onPointerDown(event), false)
+
+    this.element.oncontextmenu = function (event) {
+      event.preventDefault()
+      event.stopPropagation()
+    }
+  }
+
+  onPointerDown (event) {
+    event.preventDefault()
+    event.stopPropagation()
+    console.log('DOWN')
+
+    event.target.setPointerCapture(event.pointerId)
+    this.pointerId = event.pointerId
+
+    this.element.addEventListener('pointerup', this.upHandler, false)
     this.element.addEventListener('pointercancel', this.upHandler, false)
-    this.element.addEventListener('pointermove', this.moveHandler , false)
+    this.element.addEventListener('pointermove', this.moveHandler, false)
+
+    if (typeof this.onDown === 'function') {
+      this.onDown(event)
+    }
   }
 
   onPointerUp (event) {
-    console.log("UP!!!")
+    if (this.pointerId !== event.pointerId) {
+      console.log('returning early !')
+      return
+    }
+    this.pointerId = undefined
+
+    event.preventDefault()
+    console.log('UP!!!')
+
+    event.stopPropagation()
+    // this.element.releasePointerCapture(event.pointerId)
     this.element.removeEventListener('pointerup', this.upHandler, false)
     this.element.removeEventListener('pointercancel', this.upHandler, false)
 
-    this.upHandler = undefined
-
     this.element.removeEventListener('pointermove', this.moveHandler, false)
-    this.moveHandler = undefined
+
+    if (typeof this.onUp === 'function') {
+      this.onUp(event)
+    }
   }
 
   onPointerMove (event) {
-    console.log("MOVE!!!")
+    if (this.pointerId !== event.pointerId) {
+      return
+    }
+    event.preventDefault()
+    event.stopPropagation()
+    console.log('MOVE!!!')
+
+    if (typeof this.onMove === 'function') {
+      this.onUp(event)
+    }
   }
 }
--- static/main.js
+++ static/main.js
@@ -1,7 +1,7 @@
-/* global querySelectorAll WebSocket getComputedStyle */
+/* global WebSocket getComputedStyle */
 
 // import { TouchManager } from './touch_manager.js'
-import { Button } from './button.js'
+import { ClickButton, HoldButton } from './button.js'
 
 let current = null
 const faceButtons = document.querySelectorAll('.face')
@@ -24,57 +24,28 @@ function handleFaceButton (event) {
   console.log(face)
 }
 
-function start_look (target) {
+function startLook (event) {
+  const target = event.target
   const direction = target.textContent
   console.log('Start looking : ' + direction)
   target.style.backgroundColor = pressedColor
 }
 
-function stopLook (touch) {
-  const target = touch.target
+function stopLook (event) {
+  const target = event.target
   const direction = target.textContent
   console.log('Stop looking : ' + direction)
   target.style.backgroundColor = null
 }
 
-function handleLookTouchStart (event) {
-  console.log('TOUCH START!')
-  console.log(event2.changedTouches[0].identifier)
-  event.preventDefault()
-
-  const target = event.target
-  start_look(target)
-
-  const touch = event.changedTouches[0]
-
-  touchManager.register(touch.identifier, { onStop: stopLook })
-  // function handle_stop (touch) {
-  //  console.log(touch.identifier)
-  //  stopLook(target)
-  //  on_stop_handlers.delete(touch.identifier)
-  // }
-
-  // on_stop_handlers.set(event.changedTouches[0].identifier, handle_stop)
+function bindLookButtons (button, idx, array) {
+  new HoldButton(button, startLook, stopLook)
 }
 
-function handleLookTouchEnd (event) {
-  console.log('TOUCH END!')
-  console.log(event.changedTouches[0].identifier)
-  event.preventDefault()
+function bindFaceButtons (button, idx, array) {
+  new ClickButton(button, handleFaceButton)
 }
 
-function bind_look_handers (button, idx, array) {
-  console.log('binding ! : ' + button)
-  button.addEventListener('touchstart', handleLookTouchStart, false)
-  // button.onmousedown = handleLookTouchStart;
-
-  // button.addEventListener("touchend", handleLookTouchEnd, false);
-  // button.addEventListener("touchcancel", handleLookTouchEnd, false);
-}
-
-function bindButtons (button, idx, array) {
-  var button_ = new Button(button, {}) 
-}
-faceButtons.forEach((element, _, __) => element.onclick = handleFaceButton)
-//lookButtons.forEach(bind_look_handers)
-lookButtons.forEach(bindButtons)
+// faceButtons.forEach((element, _, __) => element.onclick = handleFaceButton)
+faceButtons.forEach(bindFaceButtons)
+lookButtons.forEach(bindLookButtons)
